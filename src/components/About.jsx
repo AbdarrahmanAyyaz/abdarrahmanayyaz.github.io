b6d600bb23ff18a9259783f5b2e598130a439c38
Abdarrahman Ayyaz <35638978+AbdarrahmanAyyaz@users.noreply.github.com>
2024-07-27 19:57:45 -0700

    added portfolio to work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       </div>
       <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
         <div className='sm:text-right text-4xl font-bold'>
-          <p>Hi. I'm Abdarrahman Ayyaz, nice to meet you. Please take a look around.</p>
+          <p>Welcome! Please take a look around.</p>
         </div>
         <div>
           <p> "CON-FO-DI"<br></br><br></br> CONsistency cultivates habits. FOcus provides drive. DIscipline builds foundation. 
--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import WorkIm from '../assets/social-media-1200x800.webp'
 import RealEst from '../assets/1714780311641.jpeg'
 import work from '../assets/gigApp.jpg'
 import dna from '../assets/dna.jpg'
+import portfolio from '../assets/portfolio.png'
 
 export const data=[
     {
@@ -12,18 +13,24 @@ export const data=[
     },
     {
         id:2,
+        name:"Portfolio React App",
+        image:portfolio,
+        github:"https://github.com/AbdarrahmanAyyaz/abdarrahmanayyaz.github.io",
+    },
+    {
+        id:3,
         name:"Experience Share(Social Media App)",
         image:WorkIm,
         github:"https://github.com/AbdarrahmanAyyaz/Social-Media-App",
     },
     {
-        id:3,
+        id:4,
         name:"DNA Sequencing with CNN models",
         image:dna,
         github:"https://github.com/AbdarrahmanAyyaz/DNA-Sequencing",
     },
     {
-        id:4,
+        id:5,
         name:"Multi Class Tumor Segmentation",
         image:RealEst,
         github:"https://github.com/AbdarrahmanAyyaz/TumorSegmentation/blob/main/README.md",
